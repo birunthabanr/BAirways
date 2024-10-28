0f898787cb378f00d545ca9227a3398a8e609b0b
W.G Kavishka Gayashan De Silva <167881650+GayashanKavishka@users.noreply.github.com>
2024-10-28 20:56:56 +0530

    Do some changes to flight Schedule

--- backend/database/connection.js
+++ backend/database/connection.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2');
 const connection = mysql.createConnection({
     host: "localhost",
     user: 'root',      // Database user
-    password: '',  // Database password
+    password: 'Gaya@123',  // Database password
     database: 'Bairways'
 });
 
